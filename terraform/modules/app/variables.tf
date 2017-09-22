25999186ccae77ea9176f23977fa020fd87d3fd7
Petr Myazin <petr.myazin@gmail.com>
2017-09-22 13:53:36 +0300

    terraform modules: app, db

---
+++ terraform/modules/app/variables.tf
@@ -0,0 +1,8 @@
+variable public_key_path {
+  description = "Path to the public key used to connect to instance"
+}
+
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-1506075690"
+}
