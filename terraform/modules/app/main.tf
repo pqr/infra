efcc89fcec1aa50d3c11f4b647f56c5947ccc950
Petr Myazin <petr.myazin@gmail.com>
2017-09-22 14:06:58 +0300

    terraform fmt

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -3,16 +3,21 @@ resource "google_compute_instance" "app" {
   machine_type = "g1-small"
   zone         = "europe-west1-b"
   tags         = ["reddit-app"]
+
   boot_disk {
     initialize_params {
       image = "${var.app_disk_image}"
-    } }
+    }
+  }
+
   network_interface {
-    network       = "default"
+    network = "default"
+
     access_config = {
       nat_ip = "${google_compute_address.app_ip.address}"
     }
   }
+
   metadata {
     sshKeys = "appuser:${file(var.public_key_path)}"
   }
@@ -21,13 +26,16 @@ resource "google_compute_instance" "app" {
 resource "google_compute_address" "app_ip" {
   name = "reddit-app-ip"
 }
+
 resource "google_compute_firewall" "firewall_puma" {
   name    = "allow-puma-default"
   network = "default"
+
   allow {
     protocol = "tcp"
     ports    = ["9292"]
   }
+
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["reddit-app"]
 }
--- terraform/modules/db/main.tf
+++ terraform/modules/db/main.tf
@@ -3,14 +3,18 @@ resource "google_compute_instance" "db" {
   machine_type = "g1-small"
   zone         = "europe-west1-b"
   tags         = ["reddit-db"]
+
   boot_disk {
     initialize_params {
       image = "${var.db_disk_image}"
-    } }
+    }
+  }
+
   network_interface {
     network       = "default"
     access_config = {}
   }
+
   metadata {
     sshKeys = "appuser:${file(var.public_key_path)}"
   }
@@ -19,12 +23,15 @@ resource "google_compute_instance" "db" {
 resource "google_compute_firewall" "firewall_mongo" {
   name    = "allow-mongo-default"
   network = "default"
+
   allow {
     protocol = "tcp"
     ports    = ["27017"]
   }
+
   # правило применимо к инстансам с тегом ...
-  target_tags   = ["reddit-db"]
+  target_tags = ["reddit-db"]
+
   # порт будет доступен только для инстансов с тегом ...
-  source_tags   = ["reddit-app"]
+  source_tags = ["reddit-app"]
 }
